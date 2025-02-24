37876dbd0ef8858d18ef0ac9ccf4e88fb75938ff
Tim Schrader <holyvoltron@gmail.com>
2025-02-24 14:03:59 -0800

    entry level hello world and hello user

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Print a message to the console
+console.log('Hello, World!~');
+
+// Greet the user with a function
+function greetUser(name) {
+    return `Hello from a function, ${name}!`;
+}
+
+// Call the function and print the result
+const greeting = greetUser('TimS');
+console.log(greeting);
